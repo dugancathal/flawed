555ad8dfd69e6e9cf26ecb38f25511aa159703be
TJ Taylor <dugancathal@gmail.com>
2013-04-03 23:54:33 -0600

    Add Addressable gem for URL validation/processing

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,2 +1,11 @@
+require 'addressable/uri'
+
 class Site < ActiveRecord::Base
+  def self.from_url(params = {})
+    url = Addressable::URI.heuristic_parse(params[:url])
+    params.merge!(url: url.to_s)
+    Site.new(params)
+  rescue Addressable::URI::InvalidURIError
+    Site.new.tap {|site| site.errors[:url] << 'The URL must be a valid HTTP(s) address'}
+  end
 end
