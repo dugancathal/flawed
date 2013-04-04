6f8fc60e806cbf808126d6b32be2d502cea233e9
TJ Taylor <dugancathal@gmail.com>
2013-04-04 00:05:34 -0600

    Add two more tests to check basic Addressable functions
    * Test scheme-less URL
    * Test invalid URL <-- SHOULDN'T BE VALID

--- test/models/site_test.rb
+++ test/models/site_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class SiteTest < ActiveSupport::TestCase
-  test 'creation by url' do
+  test 'creation by full url' do
+    site = Site.from_url url: 'http://facebook.com'
+    assert_equal 'http://facebook.com', site.url.to_s
+  end
+
+  test 'creation by scheme-less url' do
     site = Site.from_url url: 'facebook.com'
     assert_equal 'http://facebook.com', site.url.to_s
+  end
+
+  test 'creation with invalid url' do
+    site = Site.from_url url: 'http://'
+    assert site.errors.size > 0
+    assert !site.valid?
   end
 end
