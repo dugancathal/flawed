e960f26831c84688e44db8c9c9a73a76a5afb43f
TJ Taylor <dugancathal@gmail.com>
2013-04-04 00:06:59 -0600

    Add URL/Name presence validation on Site

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,8 @@
 require 'addressable/uri'
 
 class Site < ActiveRecord::Base
+  validates :url, :name, presence: true
+
   def self.from_url(params = {})
     url = Addressable::URI.heuristic_parse(params[:url])
     params.merge!(url: url.to_s)
