eab1fc9a71744466a15da3e48d31a83cdc2102eb
TJ Taylor <dugancathal@gmail.com>
2013-04-03 22:54:37 -0600

    Initial, bare-bones commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flawed::Application.config.secret_key_base = '4f0e7afe6374837a9405a5c214dd043184f085d86e665c2197ee55b331bbbfe6d0d288ab087399b709ca20373474adadb45b9ab1f01eacf9acd799628e79983e'
