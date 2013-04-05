2e1a0c73f4d82e0a431acfc382c521669c5c9957
TJ Taylor <dugancathal@gmail.com>
2013-04-05 02:38:18 -0600

    Add UsersController New page, work on create tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,6 +2,16 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   fixtures :users
+
+  def user_params
+    {
+      username: 'dugancathal',
+      password: 'secret',
+      password_confirmation: 'secret',
+      name: 'TJ Taylor',
+    }
+  end
+
   def setup
     @user = users(:dugancathal)
   end
@@ -19,5 +29,20 @@ class UsersControllerTest < ActionController::TestCase
   test 'user page assigns user' do
     get :show, {id: @user.id}
     assert_not_nil assigns(:user)
+  end
+
+  test 'gets new' do
+    get :new
+    assert_not_nil assigns(:user)
+    assert_select 'form' do
+      assert_select '#user_username'
+      assert_select '#user_password'
+    end
+  end
+
+  test 'posts create' do
+    post :create, user: user_params
+    assert_not_nil assigns(:user)
+    assert_redirected user_path(assigns(:user))
   end
 end
