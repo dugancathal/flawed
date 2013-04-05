ebbee209b8415fa0901f7d1184893336ff564730
TJ Taylor <dugancathal@gmail.com>
2013-04-04 22:26:42 -0600

    Add SessionsController tests.
    Add config getters/setters for Facebook::Config

--- lib/facebook.rb
+++ lib/facebook.rb
@@ -1,11 +1,19 @@
 module Facebook
   class Config < Struct.new(:config)
     def self.from_yaml_file(filename)
-      @@config = YAML::load_file(filename)[Rails.env]
+      self.config = YAML::load_file(filename)[Rails.env]
     end
 
     def self.[](key)
-      @@config[key]
+      config[key]
+    end
+
+    def self.config=(conf)
+      @@config = conf
+    end
+
+    def self.config
+      @@config ||= {}
     end
   end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def facebook_auth
+    {
+      provider: 'facebook',
+      uid: '1234567',
+      info: {name: 'TJ'},
+      credentials: { expires_at: (Time.now + 2.hours).to_i, token: 'AAABBBBCCC'}
+    }
+  end
+
+  test 'posts create is successful' do
+    post :create
+  end
+
+  test 'create establishes user_id in session' do
+    assert_not_nil session[:user_id]
+  end
+
+  test 'create will create or update a user' do
+    assert_not_nil assigns(:user)
+    assert_equal facebook_auth[:info][:name], assigns(:user).name
+  end
 end
