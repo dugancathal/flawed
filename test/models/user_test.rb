49028e460d9cfe4a38eebf1ace97c322a8f66d68
TJ Taylor <dugancathal@gmail.com>
2013-04-04 22:45:23 -0600

    Add rbates's from_omniauth method.  User spec passing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,13 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test 'from_omniauth should return a new user' do
     user = User.from_omniauth(facebook_auth)
-    assert_equal facebook_auth['info']['name'], user.name
+    assert_equal facebook_auth.info.name, user.name
+    assert_equal facebook_auth.provider, user.provider
+    assert_equal facebook_auth.uid, user.uid
+  end
+
+  test 'from_omniauth should return a valid user' do
+    user = User.from_omniauth(facebook_auth)
+    assert user.valid?
   end
 end
